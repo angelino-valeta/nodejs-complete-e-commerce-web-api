388c1d2c4ff9f955f69e9e330b6d6520cac799fe
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-06 10:07:13 +0100

    config enviroment variable

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+require('dotenv/config');
+
+const api =process.env.API_URL
 
 
 app.get('/', (req, res) => {
-  res.send('Hello API !!')
+  res.send('Hello API!!')
 })
 
 app.listen(3000, () => {
