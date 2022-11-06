70a9a0e7415372123ce0bd3eaa07a029773e98d1
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-06 20:42:28 +0100

    config loggers

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('node-uuid')
+
 const app = express();
 require('dotenv/config');
 
 
 const api = process.env.API_URL
 
+// Access log
+const fileAccessLog = path.join(__dirname, 'access.log');
+const accessLogStream = fs.createWriteStream(fileAccessLog, { flags: 'a' });
+
 // Middleware 
 app.use(bodyParser.json());
 
+morgan.token('id', function getId(req){
+  return req.id
+})
+
+app.use(assignId)
+// app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan(':id :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]', {stream: accessLogStream}))
 
 app.get('/', (req, res) => {
   res.send('Hello API!!')
@@ -21,8 +37,12 @@ app.get(`${api}/products`, (req, res) => {
       name: 'Product1',
     },
     {
-      id: 1,
-      name: 'Product1',
+      id: 2,
+      name: 'Product2',
+    },
+    {
+      id: 3,
+      name: 'Product3',
     }
   ]
   res.send({data: products})
@@ -33,6 +53,13 @@ app.post(`${api}/products`, (req, res) => {
   res.send(body)
 })
 
+
 app.listen(3000, () => {
   console.log('Server running http://localhost:3000');
 })
+
+
+function assignId(req, res, next){
+  req.id = uuid.v4();
+  next();
+}
