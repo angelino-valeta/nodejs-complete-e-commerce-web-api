f25eeb77e9199bdc6ab2a69b1352c2eb44c2f34a
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-06 10:45:37 +0100

    set middware body parser

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 require('dotenv/config');
 
-const api =process.env.API_URL
+
+const api = process.env.API_URL
+
+// Middleware 
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
   res.send('Hello API!!')
+})
+
+app.get(`${api}/products`, (req, res) => {
+  const products = [
+    {
+      id: 1,
+      name: 'Product1',
+    },
+    {
+      id: 1,
+      name: 'Product1',
+    }
+  ]
+  res.send({data: products})
+})
+
+app.post(`${api}/products`, (req, res) => {
+  const body = req.body
+  res.send(body)
 })
 
 app.listen(3000, () => {
