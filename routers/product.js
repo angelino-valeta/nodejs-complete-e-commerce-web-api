f475c7da32c7b2b3c08eeacbc7f2a0c2f67ebcf7
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-10 22:30:56 +0100

    Implement get product by ID

--- routers/product.js
+++ routers/product.js
@@ -52,11 +52,31 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
   try{
-    const products = await Product.find().select('name descripton image -_id');
+    const products = await Product.find().select('name descripton image');
     return res.status(200).json({success: true, data: products})
   }catch(err){
     return res.status(500).json({success: false, error: err})
   }
 });
+
+
+router.get('/:id', async(req, res) => {
+  const { id } = req.params
+
+  try{
+
+    const product = await Product.findById(id)
+
+    if(!product){
+      return res.status(400).json({ success: false, mensagem: `Cannot given Product with ID ${id}` })
+    }
+
+    return res.status(200).json({success: true, data: product});
+
+  }catch(err){
+    return res.status(500).json({success: false, error: err})
+  }
+
+})
 
 module.exports = router;
