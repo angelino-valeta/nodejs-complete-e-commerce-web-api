63565f175cbb307a2bfba8c1780e6b100329618b
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-10 14:51:55 +0100

    Set Default values

--- models/order.js
+++ models/order.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
   orderItems: [],
-  shippingAddress: [],
+  shippingAddress: [{name: String}],
   city: String,
   zip: String,
   phone: Number,
   status: String,
   totalPrice: Number,
   user: String,
-  dateOrdered: Date
+  dateOrdered: {
+    type: Date,
+    default: Date.now
+  }
 })
 
 exports.Order = mongoose.model('Order', orderSchema);
