310b168e436872e5721e76a8a00239c51cc3ea0c
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-10 14:52:34 +0100

    Complete properties of model

--- models/product.js
+++ models/product.js
@@ -1,24 +1,58 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-  name: String,
-  description: String,
-  richDescription: String,
-  image: String,
-  images: [],
-  brand: String,
-  price: {
-    type: Number,
+  name: {
+    type: String,
     required: true
   },
-  category: String,
+  description: {
+    type: String,
+    required: true
+  },
+  richDescription: {
+    type: String,
+    default: ''
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  images: [{type: String}],
+  brand: {
+    type: String,
+    default: ''
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
   countInStock: {
     type: Number,
     required: true,
+    min: 0,
+    max: 255,
   },
-  rating: Number,
-  isFeature: Boolean,
-  dateCreated: Date
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  isFeature: {
+    type: Boolean,
+    default: false
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 exports.Product = mongoose.model("Product", productSchema);
