20bd401b86bafd26ad1824e0ebf182c39121d392
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-11 09:51:20 +0100

    Changing _id key to id

--- models/category.js
+++ models/category.js
@@ -14,4 +14,12 @@ const categorySchema = mongoose.Schema({
   }
 })
 
+categorySchema.virtual('id').get(function(){
+  return this._id.toHexString();
+})
+
+categorySchema.set('toJSON', {
+  virtuals: true
+})
+
 exports.Category = mongoose.model('Category', categorySchema)
