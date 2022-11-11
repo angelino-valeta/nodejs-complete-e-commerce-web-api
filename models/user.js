825871b910400a21d5b74bd4382d9f7bf2435d4e
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-11 21:42:01 +0100

    complete user model

--- models/user.js
+++ models/user.js
@@ -1,20 +1,59 @@
 const mongoose = require('mongoose');
 
-const userSchrma = mongoose.Schema({
-  name: String,
-  email: String,
-  passwordHash: String,
-  street: String,
-  apartment: String,
-  city: String,
-  zip: String,
-  country: String,
-  phone: Number,
-  isAdmin: boolean,
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  street: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  apartment: {
+    type: String,
+    default: ''
+  },
+  city: {
+    type: String,
+    default: ''
+  },
+  zip: {
+    type: String,
+    default: ''
+  },
+  country: {
+    type: String,
+    default: ''
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
   dateCreated: {
     type: Date,
     default: Date.now
   }
 })
 
-exports.User = mongoose.model('Model', userSchrma);
+userSchema.virtual('id').get(function(){
+  return this._id.toHexString();
+})
+
+userSchema.set('toJSON', {
+  virtuals: true
+})
+
+exports.User = mongoose.model('Model', userSchema);
+exports.userSchema = userSchema;
