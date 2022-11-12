1ce5ac3f878b72c347bffbdafd90f0ac39b74c1b
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-12 12:01:10 +0100

    Protecting The Api Authentication JWT middlewar

---
+++ helpers/jwt.js
@@ -0,0 +1,22 @@
+const  expressjwt = require('express-jwt');
+
+function authJwt() {
+  const secret = process.env.SECRET;
+	const api = process.env.API_URL
+
+   return expressjwt.expressjwt({
+     secret,
+     algorithms: ["HS256"],
+   }).unless({
+		path: [
+			{url: /\api\/v1\/auth(.*)/, methods: [ 'POST', "OPTIONS"]},
+			{url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"]},
+			{url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+			{url: '/',methods: ["GET", "OPTIONS"]}
+	
+		]
+	 });
+}
+
+
+module.exports = authJwt;
