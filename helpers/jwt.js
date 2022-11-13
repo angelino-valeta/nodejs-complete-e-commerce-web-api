0bca0f595769aa307f21980987c4d5f459f4bdf9
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-13 22:31:02 +0100

    Revoked authorized

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,21 +1,30 @@
-const  expressjwt = require('express-jwt');
+const expressjwt = require("express-jwt");
 
 function authJwt() {
   const secret = process.env.SECRET;
-	const api = process.env.API_URL
+  const api = process.env.API_URL;
 
-   return expressjwt.expressjwt({
-     secret,
-     algorithms: ["HS256"],
-   }).unless({
-		path: [
-			{url: /\api\/v1\/auth(.*)/, methods: [ 'POST', "OPTIONS"]},
-			{url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"]},
-			{url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
-			{url: '/',methods: ["GET", "OPTIONS"]}
-		]
-	 });
+  return expressjwt
+    .expressjwt({
+      secret,
+      algorithms: ["HS256"],
+      isRevoked: isRevoked,
+    })
+    .unless({
+      path: [
+        { url: /\api\/v1\/auth(.*)/, methods: ["POST", "OPTIONS"] },
+        { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+        { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+        { url: "/", methods: ["GET", "OPTIONS"] },
+      ],
+    });
 }
 
+async function isRevoked(req, payload,) {
+  if (!payload.payload.isAdmin) {
+    return true;
+  }
+  return false;
+}
 
 module.exports = authJwt;
