8fd8534f2a8683d50808ca8ee0300d672960ef5a
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-16 08:35:48 +0100

    get Orders

--- models/order.js
+++ models/order.js
@@ -1,56 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const orderSchema = mongoose.Schema({
   orderItems: [
-     {
+    {
       type: mongoose.Schema.Types.ObjectId,
       ref: "OrderItem",
-      required: true
-     }
+      required: true,
+    },
   ],
-  shippingAddress: [{address: {type: String, required: true}}],
+  shippingAddress: [{ address: { type: String, required: true } }],
   city: {
     type: String,
-    required: true
+    required: true,
   },
   zip: {
     type: String,
-    required: true
+    required: true,
   },
   country: {
     type: String,
-    required: true
+    required: true,
   },
   phone: {
     type: String,
-    required: true
+    required: true,
   },
   status: {
     type: String,
     required: true,
-    default: "Pending"
+    default: "Pending",
   },
   totalPrice: {
     type: Number,
-    default: 0
+    default: 0,
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "user",
-    required: true
+    required: true,
   },
   dateOrdered: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
-orderSchema.virtual('id').get(function (){
+orderSchema.virtual("id").get(function () {
   return this._id.toHexString();
-})
+});
 
-orderSchema.set('toJSON', {
-  virtuals: true
-})
+orderSchema.set("toJSON", {
+  virtuals: true,
+});
 
-exports.Order = mongoose.model('Order', orderSchema);
+exports.Order = mongoose.model("Order", orderSchema);
--- routers/order.js
+++ routers/order.js
@@ -1,13 +1,39 @@
 const express = require("express");
-const Order = require("../models/order");
+const { Order } = require("../models/order");
 const router = express.Router();
+const mongoose = require("mongoose");
 
+router.post("/", async(req, res) => {
+	
+})
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const orders = await Order.find();
     res.status(200).json({
-        success: true,
-        data: []
-    })
+      success: true,
+      data: orders,
+    });
+  } catch (err) {
+    return res.status(500).json({ success: false, error: err });
+  }
+});
+
+
+router.get("/:id", async(req, res) => {
+	const { id } = req.params;
+
+	if(!mongoose.isValidObjectId(id)){
+		return res.status(400).json({success: false, message: "The Id is Invalid"});
+	}
+
+	try{
+		const order = await Order.findById(id);
+		return res.status(200).json({success: true, data:  order})
+	}catch(err){
+		return res.status(500).json({success: false, error: err})
+	}
+
 })
 
 module.exports = router;
