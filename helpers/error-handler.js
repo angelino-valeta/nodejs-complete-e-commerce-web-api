8688e399cca761315dbfe6cfb26889b1b537f429
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-23 06:23:57 +0100

    status code 401 unauthorized

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -1,6 +1,6 @@
 function errorHandler(err, req, res, next) {
   if (err.name === "UnauthorizedError") {
-    return res.status(400).json({ message: "The user is not authorized" });
+    return res.status(401).json({ message: "The user is not authorized" });
   }
 
   if (err.name === "ValidationError") {
