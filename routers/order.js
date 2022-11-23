411261c6f8fad428390c9357b34948f14f5e6fab
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-23 23:24:14 +0100

    Updated status order

--- routers/order.js
+++ routers/order.js
@@ -49,7 +49,12 @@ router.post("/", async (req, res) => {
       message: "Order created",
     });
   } catch (err) {
-    return res.status(500).json({ success: false, message: "Oh! Sorry something went wrong on the server" });
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: "Oh! Sorry something went wrong on the server",
+      });
   }
 });
 
@@ -57,13 +62,19 @@ router.get("/", async (req, res) => {
   try {
     const orders = await Order.find()
       // .populate({ path: "orderItems", populate: "product" })
-      .populate("user", "name email ").sort({"dateOrdered": -1});
+      .populate("user", "name email ")
+      .sort({ dateOrdered: -1 });
     res.status(200).json({
       success: true,
       data: orders,
     });
   } catch (err) {
-    return res.status(500).json({ success: false, message: "Oh! Sorry something went wrong on the server" });
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: "Oh! Sorry something went wrong on the server",
+      });
   }
 });
 
@@ -79,16 +90,59 @@ router.get("/:id", async (req, res) => {
   try {
     const order = await Order.findById(id)
       .populate("user", "name email")
-      .populate({path: "orderItems", populate: {path: "product", populate: "category"}})
+      .populate({
+        path: "orderItems",
+        populate: { path: "product", populate: "category" },
+      });
 
-
-		if(!order){
-			return res.status(404).json({success: false, message: `Cannot given Order with ID ${id}`})
-		}
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: `Cannot given Order with ID ${id}` });
+    }
 
     return res.status(200).json({ success: true, data: order });
   } catch (err) {
-    return res.status(500).json({ success: false, message: "Oh! Sorry something went wrong on the server" });
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: "Oh! Sorry something went wrong on the server",
+        error: err,
+      });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "The Id is Invalid" });
+  }
+
+  const { status } = req.body;
+
+  try {
+    const order = await Order.findByIdAndUpdate(id, { status }, { new: true });
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order cannot be updated!, not found",
+      });
+    }
+
+    return res.status(200).json({ success: true, data: order });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: "Oh! Sorry something went wrong on the server",
+        error: err,
+      });
   }
 });
 
